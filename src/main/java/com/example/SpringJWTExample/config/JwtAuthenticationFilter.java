a01df130074fecb94994a5b898fecd45d21ecba1
Linas <l.sarpauskas@gmail.com>
2023-06-03 07:46:50 +0300

    ADDED V1 of JWT

---
+++ src/main/java/com/example/SpringJWTExample/config/JwtAuthenticationFilter.java
@@ -0,0 +1,54 @@
+package com.example.SpringJWTExample.config;
+
+import com.example.SpringJWTExample.service.JwtService;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+        //iš requesto istraukiam header' (nurodom headerName); -bearer token
+        final String authHeader = request.getHeader("Authorization");
+         final String jwt;
+        final String userEmail;
+        //patikrinam ar pries tai nebuvo autentifikuotas
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7); //Nuo 7 nes B-E-A-R-E-A-R
+        userEmail = jwtService.extractUsername(jwt); //extract the userEmail from JWT token
+
+
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+                //sito reikia, update spring security context
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/com/example/SpringJWTExample/user/Role.java
@@ -0,0 +1,6 @@
+package com.example.SpringJWTExample.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
